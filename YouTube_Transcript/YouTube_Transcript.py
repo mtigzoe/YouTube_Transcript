b497b814606a9a2475bcab4a35c04132321bb4fd
mtigzoe <mloznea@g.clemson.edu>
2021-07-15 17:56:18 -0400

    Add project files.

---
+++ YouTube_Transcript/YouTube_Transcript.py
@@ -0,0 +1,18 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
+import pafy 
+import os 
+  
+source = input("Copy and paste your YouTube url: ")
+source_video_id = pafy.new(source) 
+video_id = source_video_id.videoid
+srt = YouTubeTranscriptApi.get_transcript(video_id)
+ 
+with open("subtitles.html", "w") as f:
+    
+        
+    for i in srt:
+        
+        f.write("{}\n".format(i))
+
+
